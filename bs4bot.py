8598103c197a2a88db0984ac4a773219b2ccf89e
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-01-22 12:04:19 +0300

    Add files via upload

---
+++ bs4bot.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup as bs
+import requests, telebot
+bot = telebot.TeleBot('5037312316:AAGMy-OY9B1LAfBII6ER4hFzqjAVYs0Tg1A')
+def search(q):
+    url = f'''https://invidious.snopyta.org/search?q={'+'.join(q.split(' '))}'''
+    data = requests.get(url).content.decode('utf-8')
+    links = []
+    soup = bs(data, 'lxml')
+    videos = soup.find_all('a', {'style': 'width:100%'})
+    for video in videos:
+        links.append('https://invidious.snopyta.org' + video.get('href'))
+    return links
+
+@bot.message_handler(content_types=['text'])
+def handle(message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Welcome to Invidious Search Bot by Sovenok-Hacker!")
+    else:
+        for link in search(message.text):
+            bot.send_message(message.from_user.id, link)
+bot.polling()
